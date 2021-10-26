fd0dd30ab989e094bc26e5900775d525f454293f
XanderB88 <alexxanderbykov@gmail.com>
2021-10-27 03:38:12 +0400

    Create a simple GETRequest.

--- Networking/Controllers/ViewController.swift
+++ Networking/Controllers/ViewController.swift
@@ -8,15 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     // MARK: - IBActions
     @IBAction func getRequestButton(_ sender: UIButton) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) { data, response, error in
+            
+            guard let response = response,
+                  let data = data  else {
+                      return
+                  }
+           
+            print(response)
+           
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error.localizedDescription)
+            }
+            
+            
+        }.resume()
     }
     
     @IBAction func postRequestButton(_ sender: UIButton) {
