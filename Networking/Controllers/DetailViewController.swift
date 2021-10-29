43f1eec041667afc4ea3a032805703b892379cd7
XanderB88 <alexxanderbykov@gmail.com>
2021-10-29 05:02:33 +0400

    Make a progress view and make it worked.

--- Networking/Controllers/DetailViewController.swift
+++ Networking/Controllers/DetailViewController.swift
@@ -20,19 +20,54 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = selectedCourse
         downloadWebPage(withURL: urlLink)
-        
+    }
+    
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
     }
     
     private func downloadWebPage(withURL courseURL: String) {
+        title = selectedCourse
+        
         guard let url = URL(string: courseURL) else { return }
         let request = URLRequest(url: url)
         
         webView.load(request)
         webView.allowsBackForwardNavigationGestures = true
-       
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        
+        webView.navigationDelegate = self
     }
+    
+    private func showProgressView() {
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
+            self.progressView.alpha = 1
+        }, completion: nil)
+    }
+    
+    private func hideProgressView() {
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
+            self.progressView.alpha = 0
+        }, completion: nil)
+    }
+
 }
 
-
+extension DetailViewController: WKNavigationDelegate {
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        showProgressView()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        hideProgressView()
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        hideProgressView()
+    }
+}
